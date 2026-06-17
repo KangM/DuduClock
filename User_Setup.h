a951eb022c5c80ec25d379aba8890201a66aa15b
DuduClock <dev@duduclock.local>
2026-06-17 14:14:02 +0800

    Fix board: switch to AirM2M CORE ESP32C3 with official SPI2 pins
    - FQBN: esp32:esp32:airm2m_core_esp32c3 (per LuatOS docs)
    - TFT SPI2 pins per official board spec: MOSI=3, SCLK=2, MISO=10, CS=7
    - DC=6, RST=4 (clean GPIOs)

--- User_Setup.h
+++ User_Setup.h
@@ -1,35 +1,30 @@
-// User_Setup.h for DuduClock - ST7789 240x240 on ESP32-C3
-// 此文件用于GitHub Actions编译，请根据实际硬件接线修改引脚！
+// User_Setup.h for DuduClock
+// 硬件：合宙 ESP32-C3-CORE + ST7789 240x240
+// 引脚来源：https://wiki.luatos.com/chips/esp32c3/board.html
 
 #ifndef USER_SETUP_LOADED
 #define USER_SETUP_LOADED
 
-// 驱动芯片
 #define ST7789_DRIVER
-
-// 屏幕分辨率
 #define TFT_WIDTH  240
 #define TFT_HEIGHT 240
-
-// 颜色顺序
 #define TFT_RGB_ORDER TFT_BGR
 
-// SPI引脚（ESP32-C3 默认SPI：MOSI=7, SCLK=6, MISO=2）
-// 请对照你的硬件接线修改 CS / DC / RST 引脚！
-#define TFT_MISO  2
-#define TFT_MOSI  7
-#define TFT_SCLK  6
-#define TFT_CS    10
-#define TFT_DC    8
-#define TFT_RST   4
-// #define TFT_BL    5  // 背光（如果硬件连接了）
+// SPI2 引脚（按合宙官方引脚表）
+#define TFT_MISO  10   // SPI2_MISO
+#define TFT_MOSI  3    // SPI2_MOSI
+#define TFT_SCLK  2    // SPI2_CK
+#define TFT_CS    7    // SPI2_CS
 
-// SPI频率
+// DC / RST 用空闲 GPIO（4/5/6/1 都可以）
+#define TFT_DC    6
+#define TFT_RST   4
+// #define TFT_BL  5    // 背光，按需取消注释
+
 #define SPI_FREQUENCY  40000000
 #define SPI_READ_FREQUENCY  20000000
 #define SPI_TOUCH_FREQUENCY  2500000
 
-// 字体内存优化
 #define SMOOTH_FONT
 
-#endif // USER_SETUP_LOADED
+#endif
